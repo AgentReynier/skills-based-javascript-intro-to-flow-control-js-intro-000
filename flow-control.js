8bbdd365ac3bd8bfa341075fc960fac660f5425e
Corey Chriss <excelsior89@gmail.com>
2019-05-07 22:34:34 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -1,12 +1,31 @@
 function basicTeenager(age) {
+  if (age === 13 || 14 || 15 || 16 || 17 || 18 || 19) {
+      return "You are a teenager!";
+  } else {
+      return undefined;
+  }
 
 }
 
 function teenager(age) {
+  if (age === 13 || 14 || 15 || 16 || 17 || 18 || 19) {
+    return "You are a teenager!"
+  } else {
+  return "You are not a teenager!"
+  }
 
 }
 
 function ageChecker(age) {
+  if (age === 13 || 14 || 15 || 16 || 17 || 18 || 19) {
+    return "You are a teenager!"
+
+  } if (age => 20) {
+    return "You are a grownup"
+
+  } if (age <= 12) {
+    return "You are a kid"
+  }
 
 }
 
